45e54fe0ce6798613c0b4ba28a88c5e5c4bfc5dc
Willin Wang <i@v0.md>
2023-12-24 21:39:16 +0800

    fix: :wrench: fix discord link

--- apps/web/src/lib/components/navbar.svelte
+++ apps/web/src/lib/components/navbar.svelte
@@ -22,7 +22,7 @@
 
     <div class="navbar-end">
       <a
-        href="https://discord.gg/XpQfJs4J"
+        href="https://discord.gg/sRpfQ9VVZH"
         rel="noreferrer"
         target="_blank"
         class="btn btn-ghost gap-1 normal-case">
